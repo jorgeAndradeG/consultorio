8e9422728a1f5bdbe4e271da84ad43e3f107b790
jonathanV22 <jonav878@gmail.com>
2021-05-31 05:54:20 -0400

    jeje

--- resources/views/consulta/ver-consulta.blade.php
+++ resources/views/consulta/ver-consulta.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.cliente')
 @section('main')
 <div class="overflow-x-auto">
-    <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+    <div class="min-w-screen min-h-screen flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
         <div class="w-full lg:w-5/6">
             <div class="bg-white shadow-md rounded my-6">
                 <table class="min-w-max w-full table-auto">
